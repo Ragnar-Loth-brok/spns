fd2534ede73f5c4d343cdc85e33fb0bfeec93ec5
Md Zohaib <mdzohaibcosmo@gmail.com>
2024-02-28 12:28:01 +0530

    Android fixes

--- App.tsx
+++ App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
 import RootNavigation from './app/navigation/RootNavigation';
+import { StatusBar } from 'react-native';
 
 function App(): React.JSX.Element {
   return (
-    <NavigationContainer>
-      <RootNavigation />
-    </NavigationContainer>
+    <>
+      <StatusBar translucent backgroundColor="transparent" />
+      <NavigationContainer>
+        <RootNavigation />
+      </NavigationContainer>
+    </>
   );
 }
 
--- android/app/src/main/java/com/spn/MainActivity.kt
+++ android/app/src/main/java/com/spn/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.spn
+
 import android.os.Bundle
+import android.os.Build
+import android.view.View
+import android.view.WindowManager
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -15,6 +19,16 @@ class MainActivity : ReactActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(null)
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+          window.decorView.systemUiVisibility = (
+                  View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                  View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+          )
+      } else {
+          window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+      }
+
   }
 
   /**
